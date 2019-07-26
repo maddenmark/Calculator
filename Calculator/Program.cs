b483f3d802a35062afd8b19262be7bc9e77054e2
maddenmark <53175435+maddenmark@users.noreply.github.com>
2019-07-26 16:32:13 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(10, 14)); 
+            Console.ReadLine();
+
+        }
+        static int GetMax(int num1, int num2)
+        {
+            int result;
+
+            if (num1 > num2)
+            {
+                result = num1;
+
+
+            }
+            else
+                result = num2;
+            return result;
+        }
+
+           
+
+        
+    }  
+}
